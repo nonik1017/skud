c919e07c5c4e1934c9845b6495cad05d82a2d0b6
Zhenis <zhenis.serkebaev@gmail.com>
2021-08-21 02:33:08 +0600

    feat(lib): add routings by tap

--- lib/parents.dart
+++ lib/parents.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:skud/filter_period.dart';
 
 class ParentsApp extends StatefulWidget {
   const ParentsApp({Key? key}) : super(key: key);
@@ -11,23 +10,23 @@ class ParentsApp extends StatefulWidget {
 }
 
 class ParentsState extends State<ParentsApp> {
-
   // Временные переменные только для верстки
   // Удалить после соединения с бэком
 
   List names = [
-    'Кажирахимов Е. С.', 
-    'Кажирахимов Е. С.', 
-    'Кажирахимов Е. С.', 
-    'Кажирахимов Е. С.', 
-    'Кажирахимов Е. С.', 
-    'Кажирахимов Е. С.', 
-    'Кажирахимов Е. С.', 
-    'Кажирахимов Е. С.', 
-    'Кажирахимов Е. С.', 
-    'Кажирахимов Е. С.', 
-    'Кажирахимов Е. С.', 
-    'Кажирахимов Е. С.'];
+    'Кажирахимов Е. 1.',
+    'Кажирахимов Е. 2.',
+    'Кажирахимов Е. С.',
+    'Кажирахимов Е. С.',
+    'Кажирахимов Е. С.',
+    'Кажирахимов Е. С.',
+    'Кажирахимов Е. С.',
+    'Кажирахимов Е. С.',
+    'Кажирахимов Е. С.',
+    'Кажирахимов Е. С.',
+    'Кажирахимов Е. С.',
+    'Кажирахимов Е. С.'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -35,66 +34,60 @@ class ParentsState extends State<ParentsApp> {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       home: Builder(
-        builder: (context) => Scaffold(
-          body: ListView(
-            children: [
-              for (var name in names) 
-                Container(
-                  decoration: const BoxDecoration(
-                    border: Border(bottom: BorderSide(width: 1,))
-                  ),
-                  child: ListTile(
-                    leading: Container(
-                      margin: const EdgeInsets.only(top: 5),
-                      child: Image.asset('assets/images/avatar.png')
-                    ),
-                    title: Container(
-                      margin: const EdgeInsets.only(top: 15),
-                      child: Text(
-                        name,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700
+          builder: (context) => Scaffold(
+                body: ListView(
+                  children: [
+                    for (var name in names)
+                      Container(
+                        decoration: const BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                          width: 1,
+                        ))),
+                        child: ListTile(
+                          leading: Container(
+                              margin: const EdgeInsets.only(top: 5),
+                              child: Image.asset('assets/images/avatar.png')),
+                          title: Container(
+                            margin: const EdgeInsets.only(top: 15),
+                            child: Text(
+                              name,
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          subtitle: Container(
+                            margin: const EdgeInsets.only(top: 10, bottom: 10),
+                            child: const Text(
+                              '87082624586',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black),
+                            ),
+                          ),
+                          // trailing: Container(
+                          //   width: 45,
+                          //   height: 30,
+                          //   decoration: BoxDecoration(
+                          //     borderRadius: BorderRadius.circular(8),
+                          //     color: const Color.fromRGBO(22, 14, 78, 0.05)
+                          //   ),
+                          //   child: const Center(
+                          //     child: Text(
+                          //       '5A',
+                          //       style: TextStyle(
+                          //         fontSize: 16,
+                          //         fontWeight: FontWeight.w600
+                          //       ),
+                          //     )
+                          //   ),
+                          // ),
                         ),
                       ),
-                    ),
-                    subtitle: Container(
-                      margin: const EdgeInsets.only(top: 10, bottom: 10),
-                      child: const Text(
-                        '87082624586',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black
-                        ),
-                      ),
-                    ),
-                    // trailing: Container(
-                    //   width: 45,
-                    //   height: 30,
-                    //   decoration: BoxDecoration(
-                    //     borderRadius: BorderRadius.circular(8),
-                    //     color: const Color.fromRGBO(22, 14, 78, 0.05)
-                    //   ),
-                    //   child: const Center(
-                    //     child: Text(
-                    //       '5A',
-                    //       style: TextStyle(
-                    //         fontSize: 16,
-                    //         fontWeight: FontWeight.w600
-                    //       ),
-                    //     )
-                    //   ),
-                    // ),
-                  ),
+                  ],
                 ),
-                
-            ],
-            
-          
-          ),
-        )
-      ),
+              )),
     );
   }
 }
